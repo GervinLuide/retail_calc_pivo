5fe055f31660190685b9cca36b52201ebb4c73f9
Gervin Luide <gervin.luide@khk.ee>
2022-03-31 09:45:51 +0300

    Mina kui ostja sisestan kauba koguse ja see väljastub ekraanile [Delivers #181737292]

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print( "kui sooritate ostu mis on rohkem või võrdne 5000$ saate lisa soodustust!!")
 kauba_valik = input("sisestage kaup mida soovite osta: ")
+kauba_kogus = int(input("Sisestage kauba kogus: "))
