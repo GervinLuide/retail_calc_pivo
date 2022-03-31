53bd7aeaf382b6a27b711417accd3b1175155eec
Gervin Luide <gervin.luide@khk.ee>
2022-03-31 09:52:02 +0300

    Mina, kui ostja sisestan osariigi, kus ma asun, et saaksin õige käibemaksu [Delivers #181737598]

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print( "kui sooritate ostu mis on rohkem või võrdne 5000$ saate lisa soodustust!!")
 kauba_valik = input("sisestage kaup mida soovite osta: ")
 kauba_kogus = int(input("Sisestage kauba kogus: "))
+asukoht = input("Sisestage State: ").upper()
